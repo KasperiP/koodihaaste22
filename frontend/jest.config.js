fc04d91f35226271078de56dc1aa98f94ceadd31
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2022-05-31 12:36:21 +0300

    Jest configuration

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,24 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextJest = require("next/jest");
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-  moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
-    "^@components/(.*)$": "<rootDir>/src/components/$1",
-    "^@pages/(.*)$": "<rootDir>/src/pages/$1",
-    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
-    "^@context/(.*)$": "<rootDir>/src/context/$1",
-    "^@layouts/(.*)$": "<rootDir>/src/layouts/$1",
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
   },
-  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["./jest.setup.ts"],
 };
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig);
